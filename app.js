ad4c39e37b41bf697c67bdd78b3f26b09c606699
AnniePawl <annampawl@gmail.com>
2018-08-30 12:12:46 -0700

    connected to Giphy API w/ http

--- app.js
+++ app.js
@@ -18,6 +18,36 @@ app.get('/hello-gif', function (req, res) {
   res.render('hello-gif', {gifUrl: gifUrl})
 });
 
+var http = require('http');
+
+app.get('/', function (req, res) {
+  console.log(req.query.term)
+  var queryString = "funny cat";
+  // ENCODE THE QUERY STRING TO REMOVE WHITE SPACES AND RESTRICTED CHARACTERS
+  var term = encodeURIComponent(queryString);
+  // PUT THE SEARCH TERM INTO THE GIPHY API SEARCH URL
+  var url = 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=dc6zaTOxFJmzC'
+
+  http.get(url, function(response) {
+    // SET ENCODING OF RESPONSE TO UTF8
+    response.setEncoding('utf8');
+
+    var body = '';
+
+    response.on('data', function(d) {
+      // CONTINUOUSLY UPDATE STREAM WITH DATA FROM GIPHY
+      body += d;
+    });
+
+    response.on('end', function() {
+      // WHEN DATA IS FULLY RECEIVED PARSE INTO JSON
+      var parsed = JSON.parse(body);
+      // RENDER THE HOME TEMPLATE AND PASS THE GIF DATA IN TO THE TEMPLATE
+      res.render('home', {gifs: parsed.data})
+    });
+  });
+});
+
 app.listen(3000, function () {
   console.log('Gif Search listening on port 3000!');
 });
