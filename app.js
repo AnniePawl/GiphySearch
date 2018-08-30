3867aeb39b2e8db722d9b2cdb5dfbb18ffc79771
AnniePawl <annampawl@gmail.com>
2018-08-30 11:30:10 -0700

    Handlebars.js

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
+
+
 var express = require('express');
 var app = express();
 
-app.get('/hello-Squirrel', function (req, res) {
-  res.send('Hello WSquirrel');
+var exphbs  = require('express-handlebars');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/hello-world', function (req, res) {
+  res.send('Hello World');
 });
 
 app.listen(3000, function () {
-  console.log('Gif Search listening on port localhost:3000!');
-
+  console.log('Example app listening on port 3000!');
 });
