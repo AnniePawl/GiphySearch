8e7968d52d7301b0ec7a8674f966be2d08b96ded
AnniePawl <annampawl@gmail.com>
2018-08-30 12:44:58 -0700

    refactored code

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 
 var express = require('express');
 var app = express();
+var http = require('http');
+var giphy = require('giphy-api')();
 
 var exphbs  = require('express-handlebars');
 
@@ -18,33 +20,10 @@ app.get('/hello-gif', function (req, res) {
   res.render('hello-gif', {gifUrl: gifUrl})
 });
 
-var http = require('http');
 
 app.get('/', function (req, res) {
-  console.log(req.query.term)
-  var queryString = req.query.term;
-  // ENCODE THE QUERY STRING TO REMOVE WHITE SPACES AND RESTRICTED CHARACTERS
-  var term = encodeURIComponent(queryString);
-  // PUT THE SEARCH TERM INTO THE GIPHY API SEARCH URL
-  var url = 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=dc6zaTOxFJmzC'
-
-  http.get(url, function(response) {
-    // SET ENCODING OF RESPONSE TO UTF8
-    response.setEncoding('utf8');
-
-    var body = '';
-
-    response.on('data', function(d) {
-      // CONTINUOUSLY UPDATE STREAM WITH DATA FROM GIPHY
-      body += d;
-    });
-
-    response.on('end', function() {
-      // WHEN DATA IS FULLY RECEIVED PARSE INTO JSON
-      var parsed = JSON.parse(body);
-      // RENDER THE HOME TEMPLATE AND PASS THE GIF DATA IN TO THE TEMPLATE
-      res.render('home', {gifs: parsed.data})
-    });
+  giphy.search(req.query.term, function (err, response) {
+    res.render('home', {gifs: response.data})
   });
 });
 
