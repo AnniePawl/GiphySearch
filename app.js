1c0c980d33e101f9ed6586279458004b3bb9a593
AnniePawl <annampawl@gmail.com>
2018-08-30 11:57:46 -0700

    added greetings.handlebars

--- app.js
+++ app.js
@@ -8,8 +8,14 @@ var exphbs  = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-app.get('/hello-squirrel', function (req, res) {
-  res.send('Hello Squirrel');
+app.get('/greetings/:name', function (req, res) {
+  var name = req.params.name;
+  res.render('greetings', {name: name});
+})
+
+app.get('/hello-gif', function (req, res) {
+  var gifUrl = 'http://media2.giphy.com/media/gYBVM1igrlzH2/giphy.gif'
+  res.render('hello-gif', {gifUrl: gifUrl})
 });
 
 app.listen(3000, function () {
